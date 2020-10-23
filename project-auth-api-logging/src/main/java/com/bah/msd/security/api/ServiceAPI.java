041a70d2b828f758463696930bcb3d996b99962b
user <user@this.com>
2020-10-22 21:29:49 -0400

    small changes to ServiceAPI

--- project-auth-api-logging/src/main/java/com/bah/msd/security/api/ServiceAPI.java
+++ project-auth-api-logging/src/main/java/com/bah/msd/security/api/ServiceAPI.java
@@ -4,14 +4,21 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.opentracing.Tracer;
+import io.opentracing.Span;
 
 
 @RestController
 @RequestMapping("/")
 public class ServiceAPI {
+	
+	@Autowired
+	private Tracer tracer;
 
 	public static long instanceId = new Random().nextInt();
 	public static int count = 0;
@@ -19,9 +26,12 @@ public class ServiceAPI {
 	
 	@GetMapping
 	public String healthCheck() {
+		Span span = tracer.buildSpan("health check").start();
+		span.setTag("http.status_code", 201);
 		count += 1;
 		Date date = new Date();
 		String dateformat = SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.SHORT, SimpleDateFormat.FULL).format(date);
+		span.finish();
 		return "<h3>The Authentication service is up and running!</h3>" + "<br/>Instance: " + instanceId + ", " + "<br/>DateTime: " + dateformat  + "<br/>CallCount: "+count;
 	}
 	
